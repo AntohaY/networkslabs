41bec5905a546739684757292e11605d222c6d71
AntohaY <antonyakovenko2401@gmail.com>
2019-04-15 21:53:50 +0300

    new lab4

--- lab4/client.py
+++ lab4/client.py
@@ -6,8 +6,8 @@ PORT = 7000        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
+    s.sendall(b'GET\n/index.html\nHTTP/1.1\nHost: 127.0.0.1\n\n')
     data = s.recv(1024)
     # sys.exit()
 
-print('Received', repr(data))
+print('Received: ', repr(data))
--- lab4/server.py
+++ lab4/server.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 
 HOST = ''
 PORT = 7000
@@ -30,7 +31,45 @@ while True:
     if not data:
         break
     line = data.decode('UTF-8')
-    line = line.replace("\n","")
-    conn.send(b'Message received, bye')
+    line = line.replace("\n"," ")
+    request_method = line.split(' ')[0]
+    response = ''
+    response_data = ''
+    file_requested = ''
+
+    if request_method == 'GET':
+        file_requested = line.split(' ')[1]
+
+        if file_requested == "/index.html":
+            response_data = b"<html><body><h1>Welcome!</h1></body></html>"
+        else:
+            print("File not found. Serving 404 page.")
+            response_data = b"<html><body><h1>Error 404: File not found</h1></body></html>"
+        filepath_to_serve = ".../{f}.".format(f=file_requested)
+        print("Serving web page [{fp}]".format(fp=filepath_to_serve))
+
+        response = response_data
+
+        response_code = ''
+        header = ''
+        if response_code == 200:
+            header += 'HTTP/1.1 200 OK\n'
+        elif response_code == 404:
+            header += 'HTTP/1.1 404 Not Found\n'
+
+        time_now = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+        header += 'Date: {now}\n'.format(now=time_now)
+        header += 'Server: Simple-Python-Server\n'
+        header += 'Connection: close\n\n'  # Signal that connection will be closed after completing the request
+
+        response_header = header.encode() + response
+
+        conn.send(response_header)
+
+    else:
+        print("Unknown HTTP request method: {method}".format(method=request_method))
+        conn.send(b'Unknown HTTP request method')
+
+    print("Method: {m}".format(m=request_method))
     print( line )
-    s.close()
+s.close()
