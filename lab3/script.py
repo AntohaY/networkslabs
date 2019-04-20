453cd6eff8ace0736a8d7e07a2f30d1ce8e39dac
AntohaY <antonyakovenko2401@gmail.com>
2019-04-20 14:56:58 +0300

    lmao

--- lab3/script.py
+++ lab3/script.py
@@ -1,18 +1,32 @@
 
 import socket
+import re
 
-request = b"GET / HTTP/1.1\nHost: www.google.com\n\n"
+request = b"GET / HTTP/1.1\nHost\n\n"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect(("www.google.com", 80))
+address = input("Enter address:  ")
+choice = input("1 if you want HEADER; 2 if you want BODY")
 
+s.connect(("{address}".format(address=address), 80))
+
+#s.connect(("www.google.com", 80))
 
 s.send(request)
 
 while 1:
-    buf = s.recv(1000)
+    buf = s.recv(10000)
     if not buf:
         break
-    print(buf)
+    new_buf = buf.decode('utf-8')
+    header = re.match("(.*?)<!doctype", str(new_buf), re.DOTALL)
+    body = re.search("<!doctype.*", str(new_buf), re.DOTALL)
+    # print(str(new_buf))
+    print("==================")
+    if choice == '1':
+        print(header.group(0))
+    if choice == '2':
+        print(body.group(0))
+    break
 
 s.close()
